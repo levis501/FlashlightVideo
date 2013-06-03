773e936b12091ef91c85e68671c30ff4d1781e8f
levis501 <levis501@yahoo.com>
2013-06-03 09:41:55 -0700

    Fixed preview hanging on wake from sleep on some devices

--- src/com/stevenlevis/flashlightvideo/FlashlightVideoView.java
+++ src/com/stevenlevis/flashlightvideo/FlashlightVideoView.java
@@ -1,20 +1,85 @@
 package com.stevenlevis.flashlightvideo;
 
-import android.view.MotionEvent;
-import android.view.SurfaceView;
+import java.io.IOException;
 
-public class FlashlightVideoView extends SurfaceView {
+import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class FlashlightVideoView extends SurfaceView implements SurfaceHolder.Callback {
 	MainActivity mMainActivity;
+	SurfaceHolder mHolder;
+	private Camera mCamera;
+	
+	private boolean mAutoFocusOK = false;
+	private boolean mAutoFocusInProgress = false;
 	
 	public FlashlightVideoView(MainActivity context) {
 		super(context);
 		mMainActivity = context;
+
+		mHolder = getHolder();
+		mHolder.addCallback(this);
 	}
+	
+	void setCamera(Camera camera) {
+		mAutoFocusOK = false;
+		mCamera = camera;
+		if (mCamera != null) {
+			try {
+				mCamera.setPreviewDisplay(mHolder);
+			} catch (IOException e) {
+	        	Toast.makeText(mMainActivity, "Unable to set camera preview.  Make sure nothing else is using the camera, or contact the application developer.", Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			}
+			Camera.Parameters params = mCamera.getParameters();
+			params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+			mCamera.setParameters(params);
+			requestLayout();
+		}
+	}
+	
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		mMainActivity.autoFocus();
+		if ((mCamera != null) && mAutoFocusOK && (!mAutoFocusInProgress)) {
+			mCamera.autoFocus(new AutoFocusCallback() {
+				@Override
+				public void onAutoFocus(boolean success, Camera camera) {
+					mAutoFocusInProgress = false;
+				}});
+			mAutoFocusInProgress = true;
+			return true;
+		}
 		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		setCamera(mCamera);
+		if (mCamera != null) {
+			mCamera.setDisplayOrientation(90);
+			mCamera.startPreview();
+			mAutoFocusOK = true;
+		}
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		mHolder = holder;
+		setCamera(mCamera);
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		if (mCamera != null) {
+			mCamera.stopPreview();
+		}
+		mAutoFocusOK = false;
 	}
 
 }
--- src/com/stevenlevis/flashlightvideo/MainActivity.java
+++ src/com/stevenlevis/flashlightvideo/MainActivity.java
@@ -1,87 +1,47 @@
 package com.stevenlevis.flashlightvideo;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.hardware.Camera;
-import android.hardware.Camera.AutoFocusCallback;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.widget.Toast;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends Activity implements SurfaceHolder.Callback {
+public class MainActivity extends Activity {
 
-	
 	private Camera mCamera;
-	private FlashlightVideoView mSurfaceView;
+	private FlashlightVideoView mView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mSurfaceView = new FlashlightVideoView(this);
-		mSurfaceView.getHolder().addCallback(this);
-		setContentView(mSurfaceView);
+		
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+		mView = new FlashlightVideoView(this);
+		setContentView(mView);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		mCamera = Camera.open();
+
+		mView = new FlashlightVideoView(this);
+		setContentView(mView);
+		mView.setCamera(mCamera);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
         if (mCamera != null) {
+			mView.setCamera(null);
         	mCamera.stopPreview();
             mCamera.release();
             mCamera = null;
         }
 	}
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-		Camera.Parameters params = mCamera.getParameters();
-		params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-		mCamera.setParameters(params);
-		try {
-			mCamera.setPreviewDisplay(mSurfaceView.getHolder());
-		} catch (IOException e) {
-			e.printStackTrace();
-        	Toast.makeText(this, "Unable to open a camera preview.  Make sure nothing else is using the camera, or contact the application developer.", Toast.LENGTH_LONG).show();
-        	finish();
-		}
-		mCamera.setDisplayOrientation(90);
-		mCamera.startPreview();
-	}
-
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		if (mCamera != null) {
-			try {
-				mCamera.setPreviewDisplay(holder);
-			} catch (IOException e) {
-				e.printStackTrace();
-	        	Toast.makeText(this, "Unable to set the camera preview display.  Make sure nothing else is using the camera, or contact the application developer.", Toast.LENGTH_LONG).show();
-	        	finish();
-			}
-		}
-	}
-
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		if (mCamera != null) {
-			mCamera.stopPreview();
-		}
-	}
-
-	public void autoFocus() {
-		if (mCamera != null) {
-			mCamera.autoFocus(new AutoFocusCallback() {
-				@Override
-				public void onAutoFocus(boolean success, Camera camera) {
-				}});
-		}
-	}
 }
